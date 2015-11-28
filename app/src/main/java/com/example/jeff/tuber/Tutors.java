157ba1394813ecc1f178290033724cb6756bd25c
Basil Pocklington <bg.88888@hotmail.com>
2015-11-28 16:56:31 -0500

    Updated backend

--- app/src/main/java/com/example/jeff/tuber/Tutors.java
+++ app/src/main/java/com/example/jeff/tuber/Tutors.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,16 +24,20 @@ public class Tutors extends AppCompatActivity {
         tutorSubmit.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v){
-                        TextView textView = (TextView)findViewById(R.id.textView);
-                        TextView textView3 = (TextView)findViewById(R.id.textView3);
                         EditText editTextName = (EditText)findViewById(R.id.editTextName);
                         EditText editTextRate = (EditText)findViewById(R.id.editTextRate);
 
                         Editable name = editTextName.getText();
                         Editable rate = editTextRate.getText();
 
-                        textView.setText(name);
-                        textView3.setText(rate);
+                        CheckBox checkMath = (CheckBox)findViewById(R.id.checkMath);
+                        CheckBox checkScience = (CheckBox)findViewById(R.id.checkScience);
+                        CheckBox checkEnglish = (CheckBox)findViewById(R.id.checkEnglish);
+                        CheckBox checkProgramming = (CheckBox)findViewById(R.id.checkProgramming);
+
+                        if(checkMath.equals(true)) {
+                            name.append("aaa");
+                        }
 
                         //ParseObject tutorInfo = new ParseObject("TutorInfo");
                         //gameScore.put("name", name);
