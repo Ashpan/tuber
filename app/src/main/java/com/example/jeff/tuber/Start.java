7b3afe41010f7204f3f56cf90ed5be2f09ab1a3b
JEFF <zhang.jeffreyd@gmail.com>
2015-11-28 07:09:16 -0500

    started skeleton of design

--- app/src/main/java/com/example/jeff/tuber/Start.java
+++ app/src/main/java/com/example/jeff/tuber/Start.java
@@ -2,8 +2,6 @@ package com.example.jeff.tuber;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -28,15 +26,6 @@ public class Start extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
