68854dfec1dce1db7d06b148c92a632cc37609e9
Basil Pocklington <bg.88888@hotmail.com>
2015-11-28 13:09:18 -0500

    Added ability to read text

--- app/src/main/java/com/example/jeff/tuber/Tutors.java
+++ app/src/main/java/com/example/jeff/tuber/Tutors.java
@@ -3,6 +3,10 @@ package com.example.jeff.tuber;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Tutors extends AppCompatActivity {
 
@@ -12,5 +16,16 @@ public class Tutors extends AppCompatActivity {
         setContentView(R.layout.activity_tutors);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button tutorSubmit = (Button)findViewById(R.id.tutorSubmit);
+
+        tutorSubmit.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v){
+                        TextView editText = (TextView)findViewById(R.id.editText);
+                        String name = editText.toString();
+                    }
+                }
+        );
     }
 }
