453228201725b0c9a7ca1c4ea67928fe6e60503d
Basil Pocklington <bg.88888@hotmail.com>
2015-11-28 09:46:03 -0500

    Reformatted tutor view

--- app/src/main/java/com/example/jeff/tuber/Tutors.java
+++ app/src/main/java/com/example/jeff/tuber/Tutors.java
@@ -3,6 +3,7 @@ package com.example.jeff.tuber;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Spinner;
 
 public class Tutors extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class Tutors extends AppCompatActivity {
         setContentView(R.layout.activity_tutors);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
 }
